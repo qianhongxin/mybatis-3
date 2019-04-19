674092701af4c0b15b1e8907bb58a4ca9a6d4479
32415 <hongxinqian@ehai.cn>
2019-04-19 16:28:34 +0800

    批注

--- src/main/java/org/apache/ibatis/plugin/Plugin.java
+++ src/main/java/org/apache/ibatis/plugin/Plugin.java
@@ -33,7 +33,9 @@ import org.apache.ibatis.reflection.ExceptionUtil;
 public class Plugin implements InvocationHandler {
 
   private final Object target;
+
   private final Interceptor interceptor;
+
   private final Map<Class<?>, Set<Method>> signatureMap;
 
   private Plugin(Object target, Interceptor interceptor, Map<Class<?>, Set<Method>> signatureMap) {
@@ -45,26 +47,35 @@ public class Plugin implements InvocationHandler {
   // 判断interceptor是否支持target拦截，支持则创建target的代理对象，否则原样返回target
   // 如果一个对象适配多个interceptor，则会被多次代理
   public static Object wrap(Object target, Interceptor interceptor) {
+    // 解析 interceptor 的 @interceptors 注解中定义的内容
     Map<Class<?>, Set<Method>> signatureMap = getSignatureMap(interceptor);
+
+    // 获取 target 的字节码对象
     Class<?> type = target.getClass();
+
+    // 根据 type 和 signatureMap，获取 type 查找所有匹配的拦截器支持拦截的接口字节码对象数组
     Class<?>[] interfaces = getAllInterfaces(type, signatureMap);
+
+    // 如果 interfaces 的大小大于 0，则给 target 对象创建代理对象。否则不创建代理对象
     if (interfaces.length > 0) {
-      // 创建interfaces接口的代理类对象。利用的是JDK动态代理，所以Plugin类只支持接口形式的代理对象生成，其他的形式需要自己写，不能用这个mybatis提供的Plugin类了
-      return Proxy.newProxyInstance(
-          type.getClassLoader(),
-          interfaces,
-          new Plugin(target, interceptor, signatureMap));
+
+      // 创建 target 的代理类对象。利用的是 JDK 动态代理
+      return Proxy.newProxyInstance(type.getClassLoader(), interfaces, new Plugin(target, interceptor, signatureMap));
     }
     return target;
   }
 
+  // 实现 JDK 的 InvocationHandler 接口的 invoke 方法
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
     try {
+      // 获取 method 所属类或接口的字节码对象
       Set<Method> methods = signatureMap.get(method.getDeclaringClass());
+      // 判断 methods 中是否包括 method，包含则执行 拦截器的 intercept 方法
       if (methods != null && methods.contains(method)) {
         return interceptor.intercept(new Invocation(target, method, args));
       }
+      // 调用 method 的 invoke 方法，进入下一层调用
       return method.invoke(target, args);
     } catch (Exception e) {
       throw ExceptionUtil.unwrapThrowable(e);
@@ -72,15 +83,21 @@ public class Plugin implements InvocationHandler {
   }
 
   private static Map<Class<?>, Set<Method>> getSignatureMap(Interceptor interceptor) {
-    // 获取 Intercepts
+    // 获取 @Intercepts 注解对应的字节码对象 interceptsAnnotation
     Intercepts interceptsAnnotation = interceptor.getClass().getAnnotation(Intercepts.class);
-    // issue #251
+
+    // 如果 interceptsAnnotation 为空，抛出插件定义错误的异常
     if (interceptsAnnotation == null) {
-      throw new PluginException("No @Intercepts annotation was found in interceptor " + interceptor.getClass().getName());      
+      throw new PluginException("No @Intercepts annotation was found in interceptor " + interceptor.getClass().getName());
     }
-    // 获取所有 Signature
+
+    // 获取所有 @Intercepts 中的所有 Signature
     Signature[] sigs = interceptsAnnotation.value();
+
+    // 定义 type 和 method 的映射 map
     Map<Class<?>, Set<Method>> signatureMap = new HashMap<>();
+
+    // 遍历 sigs ，初始化 signatureMap
     for (Signature sig : sigs) {
       Set<Method> methods = signatureMap.computeIfAbsent(sig.type(), k -> new HashSet<>());
       try {
@@ -91,22 +108,27 @@ public class Plugin implements InvocationHandler {
       }
     }
 
-    // 存储 接口字节码对象 和 接口方法字节码对象 的映射（对象是Executor、StatementHandler、ParameterHandler和ResultSetHandler 四个接口中的一种）
+    // 存储 接口字节码对象 和 接口方法字节码对象 的映射
     return signatureMap;
   }
 
   // 判断目标对象（Executor、StatementHandler、ParameterHandler和ResultSetHandler四个接口中的一种）是否有拦截器拦截
+  // 方法参数：  type：目标对象的字节码对象； signatureMap：是我们在 getSignatureMap 方法中得到的
   private static Class<?>[] getAllInterfaces(Class<?> type, Map<Class<?>, Set<Method>> signatureMap) {
     Set<Class<?>> interfaces = new HashSet<>();
     while (type != null) {
+      // 获取 type 的父接口，并遍历
       for (Class<?> c : type.getInterfaces()) {
+        // 判断 signatureMap 中是否包含 type 的父接口
         if (signatureMap.containsKey(c)) {
           interfaces.add(c);
         }
       }
+      // 获取 type 的父类，继续 while 循环
       type = type.getSuperclass();
     }
+
+    // 返回匹配到的所有接口字节码对象
     return interfaces.toArray(new Class<?>[interfaces.size()]);
   }
-
 }
